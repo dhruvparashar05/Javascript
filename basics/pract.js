09ad53698f66f8f33b4154e3fd07547f73e7ee05
Dhruv Parashar <dhruvparashar0505@gmail.com>
2025-07-18 10:26:34 +0000

    newfile

---
+++ basics/pract.js
@@ -0,0 +1,11 @@
+// comment //
+/* comment */
+// comment
+let name = "dhruv"
+let age = 18
+let islogin = true
+// null > empty value > typeof is object
+let state;
+// undefined > typeof is undefined
+// symbol > uniqueness
+console.log(typeof age);
