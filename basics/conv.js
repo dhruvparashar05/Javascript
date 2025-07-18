89587b3d3b2c715914ee2eec29980cf33361f078
Dhruv Parashar <dhruvparashar0505@gmail.com>
2025-07-18 11:18:59 +0000

    conv

---
+++ basics/conv.js
@@ -0,0 +1,13 @@
+let score = 33
+console.log(typeof score);
+let VarInNumber = Number(score)
+console.log(typeof VarInNumber);
+console.log(VarInNumber);
+
+// 33 > 33
+// true > 1 false > 0
+// null > 0
+// undefined > NaN(NotAnumber)
+// string > NaN
+// "" > false 
+// "dfg" > true
