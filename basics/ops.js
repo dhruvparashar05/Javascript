a67ac39de2d5b2443c56e87d70d7c6e09fcd2c63
Dhruv Parashar <dhruvparashar0505@gmail.com>
2025-07-18 11:55:11 +0000

    operation

---
+++ basics/ops.js
@@ -0,0 +1,24 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "dhruv"
+let str2 = " yo"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log(1+2+"3")
+console.log("3" + 2 + 2)
+
+let gc = 100
+gc++
+console.log(gc);
